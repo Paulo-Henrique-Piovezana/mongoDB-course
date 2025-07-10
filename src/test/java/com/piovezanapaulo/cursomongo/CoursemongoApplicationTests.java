433285221d9a0cdfc0e1775ff110e893a8ce5029
paulo henrique piovezana <paulopiovezana@gmail.com>
2025-07-10 12:14:16 -0300

    primeiro commit

---
+++ src/test/java/com/piovezanapaulo/cursomongo/CoursemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piovezanapaulo.cursomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
