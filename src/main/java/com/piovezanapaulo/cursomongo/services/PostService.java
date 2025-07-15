ebd265b67bc6676fd0f7fd71d82ec5b71dd5fc53
paulo henrique piovezana <paulopiovezana@gmail.com>
2025-07-15 17:23:23 -0300

    Consulta com varios criterios

--- src/main/java/com/piovezanapaulo/cursomongo/services/PostService.java
+++ src/main/java/com/piovezanapaulo/cursomongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.piovezanapaulo.cursomongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDat, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDat, maxDate);
 	}
 }
